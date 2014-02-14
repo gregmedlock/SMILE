57fecfec4fc8f3558005e5bdc07728712202f749
Ross Jones <jonesr18@gmail.com>
2014-02-13 19:28:12 -0800

    Added FFT function and plotting to ExampleUsage

--- Emotiv/EmotivSMILE.m
+++ Emotiv/EmotivSMILE.m
@@ -160,7 +160,7 @@ classdef EmotivSMILE < handle
 % will save this to file (as .mat)
         function recordFilename = Record(self,recordSeconds_local)
             if ~isempty(self.runAsyncTimer) && strcmp(self.runAsyncTimer.Running,'on')
-                warning('You should not run the timer and record simultaneously. Stopping the timmer') %#ok<WNTAG>
+                warning('You should not run the timer and record simultaneously. Stopping the timer') %#ok<WNTAG>
                 self.Stop()
             end
             
