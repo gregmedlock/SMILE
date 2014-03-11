9de7de1d8ce766e17e22a23f4d26fa1358e36cca
Ross Jones <jonesr18@gmail.com>
2014-03-10 19:00:21 -0700

    Added fisheriris test as known example

--- ClassificationTree_testing.m
+++ ClassificationTree_testing.m
@@ -1,3 +1,15 @@
+%% Example known to work
+close all
+clear all
+clc
+
+load fisheriris
+t = ClassificationTree.fit(meas,species,'PredictorNames',{'SL' 'SW' 'PL' 'PW'})
+view(t, 'mode', 'graph')
+
+
+%% Testing with saved data
+
 % Import data
 [X, Y] = xlsread('classification.xlsx');
 freqs = cell(1, 22);
